4f54be4f7917dedf4ff7bf6741b1f16b0f84d90c
daovanhuy209 <daovanhuy2513@gmail.com>
2024-12-17 10:10:17 +0700

    Add files via upload

---
+++ src/bacsic/dev/MainApp.java
@@ -0,0 +1,20 @@
+package bacsic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		
+		if(a > 5) {
+			System.out.println("A lon hon 5");
+		    System.out.println("Hien thi trong cau lemh if");
+	}
+		System.out.println(" Ket thuc chuong trinh");
+	}
+
+}
